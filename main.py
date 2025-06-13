f41886de62beaaba7e474f252422d73c178092a4
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-06-13 18:46:19 -0400

    initialize vuln-api

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Request
 from starlette.responses import HTMLResponse
 
+
+app = FastAPI()
+
+
 @app.get("/search", response_class=HTMLResponse)
 async def search(request: Request):
     query = request.query_params.get("q", "")
